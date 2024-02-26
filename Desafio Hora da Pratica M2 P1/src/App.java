95454c8702f7228d02082bde295182bfc2ba6907
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-02-26 15:45:17 -0300

    Passo 3

--- Desafio Hora da Pratica M2 P1/src/App.java
+++ Desafio Hora da Pratica M2 P1/src/App.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
         ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
-        
+        Pessoa fax = new Pessoa("Fax", 20);
+        Pessoa Rheider = new Pessoa("Rheider", 33);
+        Pessoa Miguel = new Pessoa("Miguel", 24);
+        listaDePessoas.add(fax);
+        listaDePessoas.add(Rheider);
+        listaDePessoas.add(Miguel);
     }
 }
